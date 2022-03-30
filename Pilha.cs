e33b83bddc5b5aa9246fb981f3f3c83c866f1faa
Taiane Rodrigues <taianeapdarodrigues@gmail.com>
2022-03-30 08:51:54 -0300

    Add files via upload

---
+++ Pilha.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha
+{
+    public class Pilha
+    {
+        /// <summary>
+        /// Aponta para o topo da pilha
+        /// </summary>
+        public NoPilha Topo { get; set; }
+
+        /// <summary>
+        /// Construtora padrão
+        /// </summary>
+        public Pilha()
+        {
+            Topo = null;
+        }
+
+        /// <summary>
+        /// Empilha o valor informado
+        /// </summary>
+        /// <param name="valor">Valor a ser empilhado</param>
+        public void Push(int valor)
+        {
+            NoPilha novo = new NoPilha(valor);
+            novo.Proximo = Topo;
+            Topo = novo;
+        }
+
+        public int Pop()
+        {
+            int valorRetorno = -1;
+
+            if (Topo != null)
+            {
+                valorRetorno = Topo.Info;
+                Topo = Topo.Proximo;
+            }
+
+            return valorRetorno;
+        }
+
+        public int Tamanho()
+        {
+            int valorRetorno = 0;
+
+
+            return valorRetorno;
+        }
+
+        public void Imprimir()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha pilha = new Pilha();
+            pilha.Push(1);
+            pilha.Push(2);
+            pilha.Push(3);
+            pilha.Push(4);
+            pilha.Push(5);
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Pop());
+            Console.WriteLine(pilha.Pop());
+        }
+    }
+}
